35a5ae237e9849f786f03ed294b9db716f755295
Aristóteles Coutinho <aristotelesbr@gmail.com>
2023-12-29 01:19:00 +0000

    fix: Reload class and update test cases

--- test/lib/lenna/middleware/default/test_reload.rb
+++ test/lib/lenna/middleware/default/test_reload.rb
@@ -4,25 +4,27 @@ require 'test_helper'
 
 require 'lenna/middleware/default/reload'
 
-module Middleware
-	module Default
-		class TestReload < Minitest::Test
-			def test_initialize
-				directories = ['test/lib/lenna/middleware/default/test_reload.rb']
-				reload      = ::Middleware::Default::Reload.new(directories)
+module Lenna
+	module Middleware
+		module Default
+			class TestReload < Minitest::Test
+				def test_initialize
+					directories = ['test/lib/lenna/middleware/default/test_reload.rb']
+					reload      = Lenna::Middleware::Default::Reload.new(directories)
 
-				assert_instance_of(::Middleware::Default::Reload, reload)
-			end
+					assert_instance_of(Lenna::Middleware::Default::Reload, reload)
+				end
 
-			def test_call
-				req = Rack::Request.new({})
-				res = Rack::Response.new
-				next_middleware = proc { res.status = 200 }
-				reload = Middleware::Default::Reload.new
+				def test_call
+					req = Rack::Request.new({})
+					res = Rack::Response.new
+					next_middleware = proc { res.status = 200 }
+					reload = Lenna::Middleware::Default::Reload.new
 
-				reload.call(req, res, next_middleware)
+					reload.call(req, res, next_middleware)
 
-				assert_equal(200, res.status)
+					assert_equal(200, res.status)
+				end
 			end
 		end
 	end
