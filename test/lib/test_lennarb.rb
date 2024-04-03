e2d54601c0788b2f01ba9e1c01454412d73b714f
Aristóteles <aristotelesbr@gmail.com>
2024-04-02 22:29:03 -0300

    test: ensure Lennarb::Router module

--- test/lib/test_lennarb.rb
+++ test/lib/test_lennarb.rb
@@ -29,6 +29,42 @@ class TestLennarb < Minitest::Test
 		assert_respond_to app, :call
 	end
 
+	class TestApp
+		include Lennarb::Router
+
+		route.get '/' do |_req, res|
+			res.status = 200
+			res.html('GET Response')
+		end
+
+		route.post '/' do |_req, res|
+			res.status = 201
+			res.html('POST Response')
+		end
+	end
+
+	def use_app(app) = Rack::Test::Session.new(app)
+
+	def test_router
+		app = use_app(TestApp.app)
+
+		app.get '/'
+
+		assert_equal 200, app.last_response.status
+		assert_equal 'GET Response', app.last_response.body
+		assert_equal 'text/html', app.last_response.headers[Rack::CONTENT_TYPE]
+	end
+
+	def test_router_post
+		app = use_app(TestApp.app)
+
+		app.post '/'
+
+		assert_equal 201, app.last_response.status
+		assert_equal 'POST Response', app.last_response.body
+		assert_equal 'text/html', app.last_response.headers[Rack::CONTENT_TYPE]
+	end
+
 	def app
 		Lennarb.new do |route|
 			route.get '/users' do |_req, res|
