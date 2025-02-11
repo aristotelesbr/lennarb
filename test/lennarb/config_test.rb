e57bfaca074b1f56ae1d65205660a440e2c2b8d4
Aristóteles <aristotelesbr@gmail.com>
2025-02-10 22:01:51 -0300

    test: ensure the behavior of the Config class

---
+++ test/lennarb/config_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class ConfigTest < Minitest::Test
+  test "overrides to_s" do
+    config = Lennarb::Config.new(env: {})
+
+    assert_equal "#<Lennarb::Config>", config.to_s
+  end
+
+  test "removes the credential method" do
+    config = Lennarb::Config.new(env: {})
+
+    refute_respond_to config, :credential
+  end
+
+  test "raises exception when mandatory key is missing" do
+    config = Lennarb::Config.new(env: {})
+
+    assert_raises Lennarb::Config::MissingEnvironmentVariable do
+      config.instance_eval do
+        mandatory :database_url, string
+      end
+    end
+  end
+
+  test "raises exception when property doesn't have callable" do
+    config = Lennarb::Config.new(env: {})
+
+    assert_raises Lennarb::Config::MissingCallable do
+      config.instance_eval do
+        property :foo
+      end
+    end
+  end
+end
