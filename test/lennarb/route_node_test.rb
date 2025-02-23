012f373391150c32a185ade67b3413eae58365f3
Aristoteles BR <aristotelesbr@gmail.com>
2025-02-23 18:44:20 -0300

    test: ensure merge! and fix software design of tests api

--- test/lennarb/route_node_test.rb
+++ test/lennarb/route_node_test.rb
@@ -2,42 +2,51 @@ require "test_helper"
 
 module Lennarb
   class RouteNodeTest < Minitest::Test
-    def setup
-      @route_node = Lennarb::RouteNode.new
+    test "add route" do
+      router = Lennarb::RouteNode.new
+      router.add_route(["posts"], "GET", proc { "List of posts" })
+      router.add_route(["posts", ":id"], "GET", proc { |id| "Post #{id}" })
 
-      @route_node.add_route(["posts"], "GET", proc { "List of posts" })
-      @route_node.add_route(["posts", ":id"], "GET", proc { |id| "Post #{id}" })
-    end
+      dynamic_node = router.static_children["posts"].dynamic_children[:id]
 
-    def test_add_route
-      assert @route_node.static_children.key?("posts")
-      dynamic_node = @route_node.static_children["posts"].dynamic_children[:id]
-
+      assert router.static_children.key?("posts")
       refute_nil dynamic_node
     end
 
-    def test_match_valid_route
-      block, params = @route_node.match_route(["posts"], "GET")
+    test "match valid route" do
+      router = Lennarb::RouteNode.new
+      router.add_route(["posts"], "GET", proc { "List of posts" })
+      router.add_route(["posts", ":id"], "GET", proc { |id| "Post #{id}" })
+
+      block, params = router.match_route(["posts"], "GET")
 
       assert_equal "List of posts", block.call
       assert_empty params
     end
 
-    def test_match_route_with_parameters
-      block, params = @route_node.match_route(%w[posts 123], "GET")
+    test "match route with parameters" do
+      router = Lennarb::RouteNode.new
+      router.add_route(["posts"], "GET", proc { "List of posts" })
+      router.add_route(["posts", ":id"], "GET", proc { |id| "Post #{id}" })
+
+      block, params = router.match_route(%w[posts 123], "GET")
 
       assert_equal "Post 123", block.call(params[:id])
       assert_equal({id: "123"}, params)
     end
 
-    def test_match_invalid_route
-      block, params = @route_node.match_route(["unknown"], "GET")
+    test "match invalid route" do
+      router = Lennarb::RouteNode.new
+      router.add_route(["posts"], "GET", proc { "List of posts" })
+      router.add_route(["posts", ":id"], "GET", proc { |id| "Post #{id}" })
+
+      block, params = router.match_route(["unknown"], "GET")
 
       assert_nil block
       assert_nil params
     end
 
-    def test_different_variables_in_common_nested_routes
+    test "handles different variable names in nested routes with common prefixes" do
       router = Lennarb::RouteNode.new
       router.add_route(["foo", ":foo"], "GET", proc { "foo" })
       router.add_route(%w[foo special], "GET", proc { "special" })
@@ -54,21 +63,50 @@ module Lennarb
       assert_equal({id: "24"}, params)
     end
 
-    def test_merge
-      router = Lennarb::RouteNode.new
-      router.add_route(["posts"], "GET", proc { "List of posts" })
-      router.add_route(["posts", ":id"], "GET", proc { |id| "Post #{id}" })
+    test "allows merging different HTTP methods on same path" do
+      router_one = Lennarb::RouteNode.new
+      router_two = Lennarb::RouteNode.new
+      router_one.add_route(["posts"], "GET", proc { "List posts" })
+      router_two.add_route(["posts"], "POST", proc { "Create post" })
 
-      router.merge!(router)
+      router_one.merge!(router_two)
+
+      assert router_one.static_children["posts"].blocks.key?("GET")
+      assert router_one.static_children["posts"].blocks.key?("POST")
     end
 
-    def test_merge_variables_in_different_routes
-      router = Lennarb::RouteNode.new
-      router.add_route(["posts"], "GET", proc { "List of posts" })
-      router.add_route(["posts", ":id"], "GET", proc { |id| "Post #{id}" })
-      router.add_route(["posts", ":id", "comments"], "GET", proc { |id| "Comments of post #{id}" })
+    test "merges nested routes correctly" do
+      router_one = Lennarb::RouteNode.new
+      router_two = Lennarb::RouteNode.new
+      router_one.add_route(["posts", "draft"], "GET", proc { "Draft posts" })
+      router_two.add_route(["posts", "published"], "GET", proc { "Published posts" })
 
-      router.merge!(router)
+      router_one.merge!(router_two)
+
+      assert router_one.static_children["posts"].static_children.key?("draft")
+      assert router_one.static_children["posts"].static_children.key?("published")
+    end
+
+    test "raises error when merging duplicate HTTP methods" do
+      router_one = Lennarb::RouteNode.new
+      router_two = Lennarb::RouteNode.new
+      router_one.add_route(["posts"], "GET", proc { "List posts" })
+      router_two.add_route(["posts"], "GET", proc { "List posts" })
+
+      assert_raises(Lennarb::RouteNode::DuplicateRouteError) do
+        router_one.merge!(router_two)
+      end
+    end
+
+    test "raises error when merging duplicate HTTP methods in nested routes" do
+      router_one = Lennarb::RouteNode.new
+      router_two = Lennarb::RouteNode.new
+      router_one.add_route(["posts", ":id"], "GET", proc { "Show post" })
+      router_two.add_route(["posts", ":id"], "GET", proc { "Show post" })
+
+      assert_raises(Lennarb::RouteNode::DuplicateRouteError) do
+        router_one.merge!(router_two)
+      end
     end
   end
 end
