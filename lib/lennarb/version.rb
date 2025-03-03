9d21bd342885a6648cbe3e0ef99e8e5b5fb8b87f
aristotelesbr <aristotelesbr@gmail.com>
2025-03-03 19:29:49 +0000

    docs: update documentation [skip ci]

--- lib/lennarb/version.rb
+++
@@ -1,3 +0,0 @@
-module Lennarb # :nodoc:
-  VERSION = "1.4.1" # :nodoc:
-end
--- test/support/lite_app/posts_controller.rb
+++
@@ -1,8 +0,0 @@
-class PostsController
-  extend Lennarb::Routes::Mixin
-
-  get "/posts" do |req, res|
-    res.status = 200
-    res.text("posts")
-  end
-end
