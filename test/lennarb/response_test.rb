ad242a8730fd13b42e6a12ef17f4c3aae8affa16
Aristoteles Costa <aristotelesbr@gmail.com>
2025-03-30 17:16:40 -0300

    test: improve test coverage

--- test/lennarb/response_test.rb
+++ test/lennarb/response_test.rb
@@ -1,64 +1,125 @@
 require "test_helper"
 
 module Lennarb
-  class TestResponse < Minitest::Test
-    test "default instance variables" do
+  class ResponseTest < Minitest::Test
+    test "default initialization" do
       response = Lennarb::Response.new
 
       assert_equal 200, response.status
-      assert_empty(response.headers)
-      assert_empty response.body
+      assert_equal({}, response.headers)
+      assert_equal [], response.body
       assert_equal 0, response.length
     end
 
-    test "set and get response header" do
+    test "setting and getting headers" do
       response = Lennarb::Response.new
 
-      response["location"] = "/"
+      response["Content-Type"] = "text/plain"
 
-      assert_equal "/", response["location"]
+      assert_equal "text/plain", response["Content-Type"]
     end
 
-    test "write to response body" do
+    test "writing to response" do
       response = Lennarb::Response.new
 
-      response.write("Hello World!")
+      response.write("Hello")
+      response.write(" World")
 
-      assert_equal "Hello World!", response.body.first
+      assert_equal ["Hello", " World"], response.body
+      assert_equal 11, response.length
+      assert_equal "11", response.headers["content-length"]
     end
 
-    test "set response status" do
+    test "writing non-string to response" do
       response = Lennarb::Response.new
 
-      response.status = 200
+      response.write(123)
 
-      assert_equal 200, response.status
+      assert_equal ["123"], response.body
+      assert_equal 3, response.length
     end
 
-    test "set response content type" do
+    test "setting text content" do
       response = Lennarb::Response.new
 
-      response["content-type"] = "text/html"
+      response.text("Hello World")
 
-      assert_equal "text/html", response["content-type"]
+      assert_equal ["Hello World"], response.body
+      assert_equal "text/plain", response.headers["content-type"]
     end
 
-    test "set response content length" do
+    test "setting html content" do
       response = Lennarb::Response.new
 
-      response["content-length"] = 12
+      response.html("<h1>Hello</h1>")
 
-      assert_equal 12, response["content-length"]
+      assert_equal ["<h1>Hello</h1>"], response.body
+      assert_equal "text/html", response.headers["content-type"]
+    end
+
+    test "setting json content" do
+      response = Lennarb::Response.new
+
+      response.json({message: "Hello", count: 42})
+
+      assert_equal 1, response.body.size
+      assert_includes response.body.first, '"message":"Hello"'
+      assert_includes response.body.first, '"count":42'
+      assert_equal "application/json", response.headers["content-type"]
+    end
+
+    test "handling json generation error" do
+      response = Lennarb::Response.new
+
+      # Mockando o JSON.generate para lançar um erro específico
+      JSON.stub :generate, ->(_) { raise JSON::GeneratorError, "Mock generator error" } do
+        response.json({test: "data"})
+
+        assert_equal 500, response.status
+        assert_equal "text/plain", response.headers["content-type"]
+        assert_includes response.body.first, "JSON generation error"
+        assert_includes response.body.first, "Mock generator error"
+      end
+    end
+
+    test "redirect response" do
+      response = Lennarb::Response.new
+
+      # Como redirect usa throw, precisamos capturar
+      caught = catch(:halt) do
+        response.redirect("/login")
+      end
+
+      status, headers, body = caught
+
+      assert_equal 302, status
+      assert_equal "/login", headers["location"]
+    end
+
+    test "redirect with custom status" do
+      response = Lennarb::Response.new
+
+      caught = catch(:halt) do
+        response.redirect("/permanent", 301)
+      end
+
+      status, headers, body = caught
+
+      assert_equal 301, status
+      assert_equal "/permanent", headers["location"]
     end
 
     test "finish response" do
       response = Lennarb::Response.new
-      response["content-type"] = "text/plain"
+      response.status = 201
+      response.headers["Content-Type"] = "text/plain"
+      response.write("Created")
 
-      response.finish
+      result = response.finish
 
-      assert_equal 0, response.length
-      assert_equal "text/plain", response["content-type"]
+      assert_equal 201, result[0]
+      assert_equal "text/plain", result[1]["Content-Type"]
+      assert_equal ["Created"], result[2]
     end
   end
 end
