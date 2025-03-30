666b9c8923e17b314d74f9fbe93182c229d1c570
Aristoteles Costa <aristotelesbr@gmail.com>
2025-03-30 17:19:23 -0300

    refactor: use SampleApp in requests tests

--- test/lennarb/requests/render_test.rb
+++ test/lennarb/requests/render_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class RenderTest < Minitest::Test
   include Rack::Test::Methods
 
-  def app = LiteApp
+  def app = SampleApp
 
   test "GET root path" do
     get "/"
@@ -35,7 +35,7 @@ class RenderTest < Minitest::Test
   end
 
   test "GET /error" do
-    LiteApp.routes.get "/error" do |_, _|
+    SampleApp.routes.get "/error" do |_, _|
       raise Lennarb::Error
     end
 
@@ -43,5 +43,34 @@ class RenderTest < Minitest::Test
 
     assert_equal 500, last_response.status
     assert_equal "Internal Server Error (Lennarb::Error)", last_response.body
+  end
+
+  test "different HTTP methods for same path" do
+    app.routes.post "/api" do |req, res|
+      res.json({action: "create"})
+    end
+
+    app.routes.get "/api" do |req, res|
+      res.json({action: "index"})
+    end
+
+    get "/api"
+    assert_equal 200, last_response.status
+    assert_equal "{\"action\":\"index\"}", last_response.body
+
+    post "/api"
+    assert_equal 200, last_response.status
+    assert_equal "{\"action\":\"create\"}", last_response.body
+  end
+
+  test "route with parameters" do
+    app.routes.get "/users/:id" do |req, res|
+      res.json({id: req.params[:id]})
+    end
+
+    get "/users/42"
+
+    assert_equal 200, last_response.status
+    assert_equal "{\"id\":\"42\"}", last_response.body
   end
 end
